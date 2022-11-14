38ccb3798811a8d29c71be099a82bd0df760458c
sagar <snkumar.nayak@gmail.com>
2022-11-14 11:46:01 +0530

    initial commit

---
+++ src/test/kotlin/com/sagar/lintertest/LinterTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sagar.lintertest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LinterTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
